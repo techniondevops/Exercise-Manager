f69a5def80f1b087110b2c06c735059fc6bdfba9
Mordonlan <77190117+Mordonlan@users.noreply.github.com>
2025-12-07 22:02:25 +0200

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -185,7 +185,7 @@ resource "aws_instance" "k8s_nodes" {
   count                  = 3
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
+  key_name = data.aws_key_pair.deployer.key_name
   subnet_id              = aws_subnet.public[count.index].id
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
